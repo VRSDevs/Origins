b432865de05c8f40bfcc80935b697a9d6bc36960
Nachete07 <ignaciocallesicq@gmail.com>
2021-06-17 19:50:27 +0200

    Escenas Victorias

--- src/js/bootloader.js
+++ src/js/bootloader.js
@@ -42,6 +42,11 @@ class bootloader extends Phaser.Scene {
         this.load.image("selectionPl2","./resources/img/MenuDesign/Selection/selectionMenu_player2.png");
         // Victoria
         this.load.spritesheet('bgVictory','./resources/img/SpriteSheet/Backgound_SpriteSheet/backgroundVictory_SpriteSheet.png' , {frameWidth: 800, frameHeight: 640} );
+        this.load.spritesheet('bgVictory_Ground','./resources/img/SpriteSheet/Backgound_SpriteSheet/VictoryGround_SpriteSheet.png' , {frameWidth: 800, frameHeight: 640} );
+        this.load.spritesheet('bgVictory_Air','./resources/img/SpriteSheet/Backgound_SpriteSheet/VictoryAir_SpriteSheet.png' , {frameWidth: 800, frameHeight: 640} );
+        this.load.spritesheet('bgVictory_Fire','./resources/img/SpriteSheet/Backgound_SpriteSheet/VictoryFire_SpriteSheet.png' , {frameWidth: 800, frameHeight: 640} );
+        this.load.spritesheet('bgVictory_Water','./resources/img/SpriteSheet/Backgound_SpriteSheet/VictoryWater_SpriteSheet.png' , {frameWidth: 800, frameHeight: 640} );
+
         // Descripción //
         this.load.image('emptyDesc', './resources/img/MenuDesign/Selection/textBox.png');
         // Mapas //
