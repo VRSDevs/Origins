73681b8280649e10ae3eef95b79b3367e92179a1
Requena21 <alejandroorequena21@gmail.com>
2021-06-19 13:09:49 +0200

    groundRoom

---
+++ src/js/menu/online/groundRoom.js
@@ -0,0 +1,99 @@
+//////////////////////////////////////////////////////////////////////
+//                  Importaciones de otros JS                       //
+//////////////////////////////////////////////////////////////////////
+import { controller } from '../gameController.js';
+import { players } from '../cats.js';
+import { game } from '../init.js';
+import { user } from '../../server/user.js';
+import { server } from '../../server/server.js';
+
+//////////////////////////////////////////////////////////////////////
+//                  Variables globales                              //
+//////////////////////////////////////////////////////////////////////
+//******************* Dimensiones lienzo ************************//
+var width = 0;      // Ancho (px)
+var height = 0;     // Alto (px)
+//****************** Botones *********************//
+var backButton = undefined;
+
+class sceneGroundRoom extends Phaser.Scene{
+    constructor() {
+        super({
+            key: "sceneGroundRoom",
+            active: false
+        });
+    }
+    create() {
+        //******************* Asignación escena ************************//  
+        controller.setCurrentScene(this);
+
+        //******************* Variables auxiliares ************************//
+        width = this.sys.canvas.width;
+        height = this.sys.canvas.height; 
+
+        //******************* Fondos ************************//
+        this.add.image(400, 320, "groundRoom");
+
+        //****************** Botones *********************//
+        // Retroceso //
+        backButton = this.add.sprite(242 / 2, 580, "spriteBackButton2", 0).setInteractive();
+        this.anims.create({
+            key: 'backButtonAnim',
+            frames: this.anims.generateFrameNumbers('spriteBackButton', {start: 1, end: 4}),
+            frameRate: 6,
+            repeat: 0
+        });
+
+        backButton.addListener('pointerover', () => {
+            lobby = 5;
+            backButton.anims.play('backButtonAnim',true);
+        }, this);
+        backButton.addListener('pointerout', () => {
+            lobby = 0;
+            backButton.anims.stop();
+            backButton.setFrame(0);
+        }, this);
+        backButton.addListener('pointerdown', loadScene, this);
+
+
+
+    }
+    update() {
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+//******************* Carga de escena ************************//
+function loadScene(){
+    switch (lobby) {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            controller.getCurrentScene().scene.stop();
+            var nextScene = game.scene.getScene("sceneMainMenu");
+            nextScene.scene.start();
+
+            break;
+    }
+}
+
