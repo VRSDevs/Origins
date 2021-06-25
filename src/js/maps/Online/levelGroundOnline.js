5ca2c3659a938905d9f51566e44934b30091b0bd
Antonio Ruano <blinx2500@gmail.com>
2021-06-25 19:14:36 +0200

    dafuq2

--- src/js/maps/Online/levelGroundOnline.js
+++ src/js/maps/Online/levelGroundOnline.js
@@ -665,6 +665,7 @@ function updatePoints() {
         //
         textPlayerPts[i].setText(Math.trunc(players[i].getScore() / diffT));
     }
+}
 
 //******************* Reseteo de variables ************************//
 function resetVariables(){
