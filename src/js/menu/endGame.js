95e252ad0e1d3d8ee2ac1e9965f94576aed583ac
Nachete07 <ignaciocallesicq@gmail.com>
2021-06-25 17:40:40 +0200

    Color letra

--- src/js/menu/endGame.js
+++ src/js/menu/endGame.js
@@ -126,29 +126,29 @@ class sceneEndGame extends Phaser.Scene {
 
                 // Texto //
                 if(textureID == "GroundCatIdle3"){
-                    victoryText = this.add.text(width/2 - 135, -20, "Player 2 won!", {
+                    victoryText = this.add.text(width/2 - 135, -20, "Player 1 won!", {
                         fontFamily: 'origins',
                         fontSize: '40px',
-                        fill: '#FF0000 '
+                        fill: '#008f39'
                     });
     
                 }else if(textureID == "WaterCatIdle3"){
-                    victoryText = this.add.text(width/2 - 135, -20, "Player 2 won!", {
+                    victoryText = this.add.text(width/2 - 135, -20, "Player 1 won!", {
                         fontFamily: 'origins',
                         fontSize: '40px',
-                        fill: '#008f39 '
+                        fill: '#0000ff'
                     });
                 }else if(textureID == "AirCatIdle3"){
-                    victoryText = this.add.text(width/2 - 135, -20, "Player 2 won!", {
+                    victoryText = this.add.text(width/2 - 135, -20, "Player 1 won!", {
                         fontFamily: 'origins',
                         fontSize: '40px',
-                        fill: '#81CECF '
+                        fill: '#81CECF'
                     });
                 }else if(textureID == "FireCatIdle3"){
-                    victoryText = this.add.text(width/2 - 135, -20, "Player 2 won!", {
+                    victoryText = this.add.text(width/2 - 135, -20, "Player 1 won!", {
                         fontFamily: 'origins',
                         fontSize: '40px',
-                        fill: '#008f39 '
+                        fill: '#FF0000'
                     });
                 }
                 this.tweens.add({
@@ -231,26 +231,26 @@ class sceneEndGame extends Phaser.Scene {
                 victoryText = this.add.text(width/2 - 135, -20, "Player 2 won!", {
                     fontFamily: 'origins',
                     fontSize: '40px',
-                    fill: '#FF0000 '
+                    fill: '#008f39'
                 });
 
             }else if(textureID == "WaterCatIdle3"){
                 victoryText = this.add.text(width/2 - 135, -20, "Player 2 won!", {
                     fontFamily: 'origins',
                     fontSize: '40px',
-                    fill: '#008f39 '
+                    fill: '#0000ff'
                 });
             }else if(textureID == "AirCatIdle3"){
                 victoryText = this.add.text(width/2 - 135, -20, "Player 2 won!", {
                     fontFamily: 'origins',
                     fontSize: '40px',
-                    fill: '#81CECF '
+                    fill: '#81CECF'
                 });
             }else if(textureID == "FireCatIdle3"){
                 victoryText = this.add.text(width/2 - 135, -20, "Player 2 won!", {
                     fontFamily: 'origins',
                     fontSize: '40px',
-                    fill: '#008f39 '
+                    fill: '#FF0000'
                 });
             }
 
